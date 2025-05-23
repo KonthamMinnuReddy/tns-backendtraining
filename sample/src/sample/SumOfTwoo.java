e418464283de347d47228c0c40552eda7eb53932
Kminnureddy <konthamminnureddy@gmail.com>
2025-05-23 11:04:36 +0530

    inserted sumof2 program

---
+++ sample/src/sample/SumOfTwoo.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class SumOfTwoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+		
+		int a=4;
+		int b=7;
+		int sum=a+b;
+		System.out.println(sum);
+	}
+
+}
